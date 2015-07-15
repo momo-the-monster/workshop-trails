8536aef32fb3e4f03f3549b2d2cb17971438d1a5
Surya <Buchwald@heliosinteractive.com>
2015-07-14 23:02:49 -0700

    add whitespace to CameraFollow

--- Assets/PDXcc/Trails/Code/CameraFollow.cs
+++ Assets/PDXcc/Trails/Code/CameraFollow.cs
@@ -7,8 +7,10 @@ namespace pdxcc
     {
         // Target for the camera to follow
         public Transform target;
+
         // Distance to maintain between camera and target
         public Vector3 offset;
+
         // How quickly to follow
         public float speed = 1f;
 
@@ -16,6 +18,7 @@ namespace pdxcc
         {   
             // Get the position of the target, add the offset
             Vector3 targetPosition = target.position + offset;
+
             // Lerp between the current position and the target position using deltaTime for consistency
             transform.position = Vector3.Lerp(transform.position, targetPosition, speed * Time.deltaTime);
         }
