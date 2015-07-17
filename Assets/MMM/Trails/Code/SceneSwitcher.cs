ff5daf064a811bced9aa07e1a20f118950c0bcfd
Surya <Buchwald@heliosinteractive.com>
2015-07-16 22:54:40 -0700

    Refactor to MMM

---
+++ Assets/MMM/Trails/Code/SceneSwitcher.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/**
+ * Use Keyboard to switch between all scenes in build
+ * Wraps from last Scene back to first and vice versa
+ */
+
+namespace mmm {
+
+    public class SceneSwitcher : MonoBehaviour {
+
+        public KeyCode keyNext = KeyCode.Equals;
+        public KeyCode keyPrevious = KeyCode.Minus;
+	
+	    void Update () {
+
+            // Trigger Scene Switch with Keyboard Commands
+            if (Input.GetKeyDown(keyNext))
+            {
+                // Increment Scene
+                int nextLevel = Application.loadedLevel;
+                nextLevel++;
+                if (nextLevel >= Application.levelCount)
+                {
+                    nextLevel = 0;
+                }
+                Application.LoadLevel(nextLevel);
+            }
+
+            if (Input.GetKeyDown(keyPrevious))
+            {
+                // Decrement Scene
+                int nextLevel = Application.loadedLevel;
+                nextLevel--;
+                if (nextLevel < 0)
+                {
+                    nextLevel = Application.levelCount;
+                }
+                Application.LoadLevel(nextLevel);
+            }
+
+	    }
+
+    }
+
+}
---
+++ Assets/MMM/Trails/Shaders/MaskedTint.shader
@@ -0,0 +1,35 @@
+﻿Shader "Unlit/Texture Tint" {
+
+Properties {
+	_Color ("Tint Color", Color) = (1,1,1)
+	_MainTex ("Texture  (A = Tint Mask)", 2D) = ""
+}
+
+SubShader {Pass {	// iPhone 3GS and later
+	GLSLPROGRAM
+	varying mediump vec2 uv;
+	
+	#ifdef VERTEX
+	void main() {
+		gl_Position = gl_ModelViewProjectionMatrix * gl_Vertex;
+		uv = gl_MultiTexCoord0.xy;
+	}
+	#endif
+	
+	#ifdef FRAGMENT
+	uniform lowp sampler2D _MainTex;
+	uniform lowp vec3 _Color;
+	void main() {
+		vec4 texture = texture2D(_MainTex, uv);
+		gl_FragColor = vec4(texture.rgb * (_Color * texture.a + (1. - texture.a)), 1);
+	}
+	#endif		
+	ENDGLSL
+}}
+
+SubShader {Pass {	// pre-3GS devices, including the September 2009 8GB iPod touch
+	SetTexture[_MainTex] {Combine texture alpha * one - constant ConstantColor[_Color]}				
+	SetTexture[_MainTex] {Combine texture * one - previous}
+}}
+
+}
