280d2aa4e4813dc689139d1639ba598d2bb39240
Surya <Buchwald@heliosinteractive.com>
2015-07-19 14:20:51 -0700

    add comments to SetLabelFromBlendmode

--- Assets/MMM/Trails/Scripts/SetLabelFromBlendmode.cs
+++ Assets/MMM/Trails/Scripts/SetLabelFromBlendmode.cs
@@ -2,6 +2,10 @@
 using System.Collections;
 using UnityEngine.UI;
     
+/*
+ * Listen for MomoMirror.OnSwitched Event
+ * Update Text Component with new Blend Mode
+ */
 namespace mmm {
 
     [RequireComponent(typeof(Text))]
@@ -9,11 +13,14 @@ namespace mmm {
 
         internal Text field;
 
+        // Add and Remove the Event Listener
         void OnEnable()  { MomoMirror.OnSwitched += OnSwitched; }
         void OnDisable() { MomoMirror.OnSwitched -= OnSwitched; }
 
+        // Cache the Text Component
         void Start() { field = GetComponent<Text>(); }
 
+        // Set Text on Event Trigger
 	    void OnSwitched ( MomoMirror.BlendModes b ) {
             field.text = "Blend: " + b.ToString();
         }
